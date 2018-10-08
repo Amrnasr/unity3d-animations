fad63cab53640fe66c36b1f09f9564b1704f4bf9
Amr Nasr <amrnasr@gmail.com>
2018-10-08 20:53:09 +0200

    Add files via upload

---
+++ AnimationController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour {
+
+
+    Animator animator;
+
+    bool running = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Horizontal"))
+        {
+            if (!running)
+            {
+                animator.SetTrigger("Run");
+                running = true;
+            }
+        }
+
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            animator.SetTrigger("Idle");
+            running = false;
+        }
+
+    }
+}
