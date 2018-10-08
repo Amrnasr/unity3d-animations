a278d98e3b4f70dfeeca72034a0b6a51b1cc727a
Amr Nasr <amrnasr@gmail.com>
2018-10-08 20:32:32 +0200

    Add files via upload
    files attacheee

---
+++ animcyborg.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animcyborg : MonoBehaviour {
+
+    //Fetch the Animator
+    Animator m_Animator;
+    // Use this for deciding if the GameObject can jump or not
+    bool m_Jump;
+
+    bool Isjumping = false;
+    //102534
+
+    bool Ispistol = false;
+
+    bool Ispresswall = false;
+
+    void Start()
+    {
+        //This gets the Animator, which should be attached to the GameObject you are intending to animate.
+        m_Animator = gameObject.GetComponent<Animator>();
+        // The GameObject cannot jump
+        m_Jump = false;
+    }
+
+
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        //Click the mouse or tap the screen to change the animation
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    m_Jump = true;
+        //    Debug.Log("i am jumping");
+        //}
+        ////Otherwise the GameObject cannot jump.
+        //else m_Jump = false;
+
+        //if (m_Jump==true)
+        //Debug.Log(m_Jump);
+
+        //If the GameObject is not jumping, send that the Boolean “Jump” is false to the Animator. The jump animation does not play.
+        //if (m_Jump == false)
+        //    m_Animator.SetBool("Isjumping", false);
+
+        //The GameObject is jumping, so send the Boolean as enabled to the Animator. The jump animation plays.
+        //if (m_Jump == true)
+          //  m_Animator.SetBool("Isjumping", true);
+
+
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+
+            Isjumping = true;
+            m_Animator.SetBool("Isjumping", Isjumping);
+
+            Isjumping = false;
+
+        }
+
+        ////if (Input.GetKeyDown(KeyCode.Keypad1))
+        ////{
+        ////    m_Animator.SetBool("Ispresswall", true);
+
+        ////}
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Ispistol = true;
+            m_Animator.SetBool("Ispistol", Ispistol);
+            Ispistol = false;
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.Keypad1))
+        {
+            //Ispresswall = true;
+            m_Animator.SetBool("Ispresswall", true);
+
+            Debug.Log("I am playing press wall");
+            if (m_Animator.GetBool("Idle"))
+            {
+                //If the "Crouch" parameter is enabled, disable it as the Animation should no longer be crouching
+                m_Animator.SetBool("Ispresswall", false);
+                Debug.Log("I have disabled the animation");
+            }
+            
+            //Ispresswall = false;
+
+        }
+
+
+        if (Input.GetKeyUp(KeyCode.Space))
+            print("space key was released");
+
+        //anim.SetBool("Kicking", Kicking);
+        //if (Input.GetButtonDown("Fire1"))
+        //{
+        //    Kicking = true;
+        //}
+
+
+
+        //if (Input.GetButtonDown("Jump"))
+        //{
+
+        //    m_Animator.SetBool("Jump", true);
+
+        //}
+
+
+	}
+}
---
+++ animcyborg2.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animcyborg2 : MonoBehaviour {
+
+
+    Animator animator;
+
+    bool jumping = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            jumping = true;
+
+
+
+            animator.SetBool("Isjumping", true);
+
+  //          Debug.Log("I am jumping");
+           // WaitAndJump();
+
+           
+        }
+       /// Debug.Log(animator.GetCurrentAnimatorStateInfo(0).IsName("Idle")); outputs false
+
+        if (animator.GetBool("Idle"))
+        {
+            //If the "Crouch" parameter is enabled, disable it as the Animation should no longer be crouching
+            animator.SetBool("Crouch", false);
+        }
+
+
+
+
+        if (jumping)
+        {
+            Debug.Log("Jumping");
+            //Debug.Log(animator.GetCurrentAnimatorClipInfo(0));
+        }
+
+
+        //if (animator.GetCurrentAnimatorStateInfo(0).IsName("Isjumping"))
+        //{
+
+        //    Debug.Log("Animation is playing");
+        //   // animator.SetBool("Isjumping", false);
+            
+        //}else
+        //{
+        //    //Debug.Log("animation Stopped");
+        //}
+
+
+    }
+
+
+    IEnumerator WaitAndJump()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        animator.SetBool("Isjumping", false);
+    }
+
+    void SetAnimateFalse()
+    {
+        animator.SetBool("Isjumping", false);
+    } 
+}
